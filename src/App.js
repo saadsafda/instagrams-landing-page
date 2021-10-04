174eb83d0af64767f4321f6e30fe2121c5bdff73
saadsafdar <72243425+saadsafda@users.noreply.github.com>
2021-10-04 07:42:01 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-  
-import React from "react";
+import React, {Suspense, lazy} from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Model from "./components/Model";
-import Stories from "./components/Stories";
-import Create from "./components/Create";
-import Posts from "./components/Posts";
-import Sidebar from "./components/Sidebar";
 import Context from "./Global/Context";
+const Navbar = lazy(() => import('./components/Navbar'));
+const Model = lazy(() => import('./components/Model'));
+const Stories = lazy(() => import('./components/Stories'));
+const Create = lazy(() => import('./components/Create'));
+const Posts = lazy(() => import('./components/Posts'));
+const Sidebar = lazy(() => import('./components/Sidebar'));
+
 function App() {
   return (
+    <Suspense fallback="loading..." >    
     <Context>
       <Navbar />
       <div className="container">
@@ -20,6 +21,8 @@ function App() {
       </div>
       <Model />
     </Context>
+    </Suspense>
+
   );
 }
 
